f6a56a98909228370bb7e9b31431bf1730f94eeb
FernandoProa <45346347+FernandoProa@users.noreply.github.com>
2018-11-25 23:12:04 -0500

    Prueba
    pito

---
+++ app/src/main/java/bfit22/fernando/com/bfit/MePicaUnHuevo.java
@@ -0,0 +1,13 @@
+package bfit22.fernando.com.bfit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MePicaUnHuevo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_me_pica_un_huevo);
+    }
+}
