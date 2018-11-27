732af10e206a890e3ea207b9750d2398d7db37c8
andy <andy@users.noreply.github.com>
2018-11-27 00:23:36 -0500

    comentarios

--- app/src/main/java/bfit22/fernando/com/bfit/RutinaAleatoria.java
+++ app/src/main/java/bfit22/fernando/com/bfit/RutinaAleatoria.java
@@ -9,7 +9,9 @@ import java.util.Random;
 
 public class RutinaAleatoria extends AppCompatActivity {
 
+    //Instancia de la clase Random que generará aleatoreamente el contenido de un ImageView
     private static final Random rgenerador = new Random();
+    //Array de contenido de rutinas
     private static final Integer[] imagenesID =
             {R.drawable.rutina1, R.drawable.rutina2, R.drawable.rutina3, R.drawable.rutina4, R.drawable.rutina5};
 
@@ -18,9 +20,14 @@ public class RutinaAleatoria extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rutina_aleatoria);
 
+        //Variable que almacenará el contenido del Imageview
         Integer q = imagenesID[rgenerador.nextInt(imagenesID.length)];
+        //Link a las propiedades del Imageview
         final ImageView iv = (ImageView) findViewById(R.id.imgRandom);
+
+        //Evento onClick que genrará las rutinas
         View nextButton = findViewById(R.id.btnNextImg);
+        
         nextButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View V) {
                 int resource = imagenesID[rgenerador.nextInt(imagenesID.length)];
