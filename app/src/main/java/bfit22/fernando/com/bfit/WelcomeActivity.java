1e42ae32d95b177cbb113d93a792c4b021e1e3c8
FernandoProa <45346347+FernandoProa@users.noreply.github.com>
2018-11-27 02:25:25 -0500

    back e imagenes
    ola wapos

--- app/src/main/java/bfit22/fernando/com/bfit/WelcomeActivity.java
+++ app/src/main/java/bfit22/fernando/com/bfit/WelcomeActivity.java
@@ -5,17 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class WelcomeActivity extends AppCompatActivity {
-    private Button btn1;
+
+    private  ImageButton btnInicio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        btn1=(Button) findViewById(R.id.buttonMain);
+
+        btnInicio=(ImageButton)findViewById(R.id.imageButtoninicio);
 
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+
+
+        btnInicio.setOnClickListener(new View.OnClickListener() {
 
 
             @Override
