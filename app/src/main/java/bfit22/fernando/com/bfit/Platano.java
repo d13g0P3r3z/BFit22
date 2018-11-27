0b2bc7817de7c27dd5e1a8aac72c72ea14580efa
d13g0P3r3z <45344988+d13g0P3r3z@users.noreply.github.com>
2018-11-26 23:22:52 -0500

    adicion de clases para spinners

---
+++ app/src/main/java/bfit22/fernando/com/bfit/Platano.java
@@ -0,0 +1,13 @@
+package bfit22.fernando.com.bfit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Platano extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_platano);
+    }
+}
